8d793bbbe5d9d9e427cc6d771ac59634aacc4f2e
Pedro Young <YoungP@wedbush.com>
2022-04-20 08:55:41 -0400

    update colors

--- src/shared/styles.js
+++ src/shared/styles.js
@@ -1,6 +1,16 @@
 export const Colors = {
-  primary: 'red',
+  "Brand3": "#00245D",
+  "Primary50": "#1F5EB7",
+  "Primary80": "#002698",
+  "NeutralWhite": "#FFFFFF",
+  "Gray10": "#F0F2F5",
+  "Gray60": "#596579",
+  "Gray80": "#192638",
+  "Elevation01": "#00245D",
+  "Success2": "#22C373",
+  "Negative2": "#C72F24",
 }
+
 export const Typography = {
   primary: 'red',
 }
