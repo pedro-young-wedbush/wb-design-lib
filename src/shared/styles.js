8c2d8d5699c19b3b3d0d6d8daac3520108039fae
Pedro Young <YoungP@wedbush.com>
2022-04-20 10:59:38 -0400

    cleaned by pretty

--- src/shared/styles.js
+++ src/shared/styles.js
@@ -1,14 +1,14 @@
 export const Colors = {
-  "Brand3": "#00245D",
-  "Primary50": "#1F5EB7",
-  "Primary80": "#002698",
-  "NeutralWhite": "#FFFFFF",
-  "Gray10": "#F0F2F5",
-  "Gray60": "#596579",
-  "Gray80": "#192638",
-  "Elevation01": "#00245D",
-  "Success2": "#22C373",
-  "Negative2": "#C72F24",
+  Brand3: '#00245D',
+  Primary50: '#1F5EB7',
+  Primary80: '#002698',
+  NeutralWhite: '#FFFFFF',
+  Gray10: '#F0F2F5',
+  Gray60: '#596579',
+  Gray80: '#192638',
+  Elevation01: '#00245D',
+  Success2: '#22C373',
+  Negative2: '#C72F24',
 }
 
 export const Typography = {
